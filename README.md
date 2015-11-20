262c324200fde72f63cfafcfd1724efddadac5ef
Ivan Yurov <ivan.youroff@gmail.com>
2015-11-20 00:04:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# ember-cli-deploy-simply-ssh #
+
+**Install**
+
+```
+$ ember install ember-cli-deploy
+$ ember install ember-cli-build
+$ ember install ember-cli-revision-data
+$ ember install ember-cli-deploy-gzip
+$ ember install ember-cli-deploy-simply-ssh
+```
+
+Sample `deploy.js`:
+
+```js
+module.exports = function(deployTarget) {
+  var ENV = {};
+
+  if (deployTarget === 'production') {
+    ENV.build = {
+      environment: 'production'
+    }
+    
+    ENV.ssh = {
+      host: process.env.SSH_HOST,
+      dir: process.env.SSH_DIR,
+      port: process.env.SSH_PORT,
+      user: process.env.SSH_USER,
+      key: process.env.SSH_KEY
+    }
+  }
+
+  return ENV;
+};
+
+```
+
+Sample `.env.deploy.production`:
+```
+SSH_HOST=yourhost.com
+SSH_DIR=/var/www/appdir
+SSH_PORT=22
+SSH_USER=deploy
+SSH_KEY=/home/user/.ssh/id_rsa
+```
+* Also possible to use password and passphrase as params.
+
+## Directory Structure ##
+
+Application releases live in `releases` dir:
+```
+/var/www/appdir/releases/35h23jh23j52k3jg5k32jh5
+/var/www/appdir/releases/2lkjs9d93ukhf3798oasjf7
+```
+Current release linked to `/var/www/appdir/current`
+index.html and assets copied together to be served by Nginx.
---
+++ index.js
@@ -0,0 +1,76 @@
+/* jshint node: true */
+'use strict';
+
+var SSH = require('./lib/ssh');
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+
+module.exports = {
+  name: 'ember-cli-deploy-ssh',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+
+      requiredConfig: ['host', 'port', 'dir'],
+      defaultConfig: {
+        host: 'localhost',
+        port: 22,
+        dir: '/var/www',
+        releasesDir: function(context) {
+          return this.readConfig('dir') + '/releases';
+        },
+        fullDistDir: function(context) {
+          return [process.cwd(), context.distDir].join('/');
+        }
+      },
+
+      setup: function(context) {
+        console.log(this.readConfig('fullDistDir'));
+        this.conn = new SSH({
+            host:       this.readConfig('host'),
+            port:       this.readConfig('port'),
+            username:   this.readConfig('user'),
+            password:   this.readConfig('password'),
+            passphrase: this.readConfig('passphrase'),
+            key:        this.readConfig('key')
+        }, this);
+        return Promise.resolve(this.conn.ssh).then(function(){
+          this.log("SSH connection established.", {color: "green"})
+        }.bind(this));
+      },
+
+      teardown: function(context) {
+        return this.conn.close();
+      },
+
+      prepare: function(context) {
+        var releaseDir = this._releaseDir(context);
+        return this.conn.createDir(releaseDir);
+      },
+
+      upload: function(context) {
+        var files = context.gzippedFiles || context.distFiles;
+        var distDir = this.readConfig('fullDistDir');
+        return this.conn.uploadFiles(files, distDir, this._releaseDir(context));
+      },
+
+      didUpload: function(context) {
+        var source = this._releaseDir(context);
+        var target = this.readConfig('dir') + '/current';
+        return this.conn.updateLink(source, target);
+      },
+
+      _revisionKey: function(context) {
+        return context.revisionData.revisionKey;
+      },
+
+      _releaseDir: function(context) {
+        return this.readConfig('releasesDir') + '/' + this._revisionKey(context);
+      }
+
+    });
+    
+    
+    return new DeployPlugin();
+  }
+};
